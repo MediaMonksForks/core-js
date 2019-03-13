111444dac73b7a3466b8957f4d931dec93cbbb39
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2019-03-13 20:11:53 +0100

    Move #sticky getter to prototype

--- packages/core-js/modules/es.regexp.constructor.js
+++ packages/core-js/modules/es.regexp.constructor.js
@@ -12,7 +12,10 @@ var redefine = require('../internals/redefine');
 var fails = require('../internals/fails');
 var setSpecies = require('../internals/set-species');
 var wellKnownSymbol = require('../internals/well-known-symbol');
+var InternalStateModule = require('../internals/internal-state');
 
+var getInternalState = InternalStateModule.get;
+var setInternalState = InternalStateModule.set;
 var MATCH = wellKnownSymbol('match');
 var NativeRegExp = global.RegExp;
 var RegExpPrototype = NativeRegExp.prototype;
@@ -60,10 +63,7 @@ if (FORCED) {
       RegExpWrapper
     );
 
-    if (UNSUPPORTED_Y) defineProperty(result, 'sticky', {
-      configurable: true,
-      get: function () { return sticky; }
-    });
+    if (UNSUPPORTED_Y) setInternalState(result, { sticky: sticky });
 
     return result;
   };
@@ -80,7 +80,22 @@ if (FORCED) {
   RegExpPrototype.constructor = RegExpWrapper;
   RegExpWrapper.prototype = RegExpPrototype;
   redefine(global, 'RegExp', RegExpWrapper);
-  if (UNSUPPORTED_Y) hide(RegExpWrapper, 'sham', true);
+
+  if (UNSUPPORTED_Y) {
+    hide(RegExpWrapper, 'sham', true);
+    defineProperty(RegExpPrototype, 'sticky', {
+      configurable: true,
+      get: function () {
+        // We can't use InternalStateModule.getterFor because
+        // we don't add metadata for regexps created by a literal.
+        if (!(this instanceof RegExpWrapper) && this !== RegExpPrototype) {
+          throw TypeError('Incompatible receiver, RegExp required');
+        }
+        var state = getInternalState(this);
+        return state.sticky;
+      }
+    });
+  }
 }
 
 // https://tc39.github.io/ecma262/#sec-get-regexp-@@species
--- packages/core-js/modules/es.regexp.flags.js
+++ packages/core-js/modules/es.regexp.flags.js
@@ -1,10 +1,11 @@
 var DESCRIPTORS = require('../internals/descriptors');
+var STICKY_HELPERS = require('../internals/regexp-sticky-helpers');
 var objectDefinePropertyModule = require('../internals/object-define-property');
 var regExpFlags = require('../internals/regexp-flags');
 
 // `RegExp.prototype.flags` getter
 // https://tc39.github.io/ecma262/#sec-get-regexp.prototype.flags
-if (DESCRIPTORS && /./g.flags != 'g') {
+if (DESCRIPTORS && (/./g.flags != 'g' || STICKY_HELPERS.UNSUPPORTED_Y)) {
   objectDefinePropertyModule.f(RegExp.prototype, 'flags', {
     configurable: true,
     get: regExpFlags
